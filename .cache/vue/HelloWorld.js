c7458375404ba6bdeb20659e6cb380e0d918c6e7
Sean Zhai <hszhai@syr.edu>
2022-02-09 23:37:40 -0500

    Delete .cache/vue directory

--- .cache/vue/HelloWorld.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var HelloWorld_vue_vue_type_script_setup_true_lang = require('./HelloWorld.vue_vue_type_script_setup_true_lang.js');
-
-HelloWorld_vue_vue_type_script_setup_true_lang.__scopeId = "data-v-469af010";
-HelloWorld_vue_vue_type_script_setup_true_lang.__file = "src/components/HelloWorld.vue";
-
-module.exports = HelloWorld_vue_vue_type_script_setup_true_lang;
