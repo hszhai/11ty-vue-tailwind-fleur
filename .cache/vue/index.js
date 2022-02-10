0b1ece84f52646a2b74bcf4159c0b153ce9c74db
Sean Zhai <hszhai@syr.edu>
2022-02-10 09:23:40 -0500

    first commit

---
+++ .cache/vue/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var index_vue_vue_type_template_id_15667630_lang = require('./index.vue_vue_type_template_id_15667630_lang.js');
+
+const script = {};
+
+
+script.ssrRender = index_vue_vue_type_template_id_15667630_lang.ssrRender;
+script.__file = "index.vue";
+
+module.exports = script;
